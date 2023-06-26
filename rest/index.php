13b7417fdce4a35e63b870176c18470bdc07185d
AhmedCosovic <ahmedcosovic6@gmail.com>
2023-06-26 02:48:58 +0200

    Swagger files and some configuration

--- rest/index.php
+++ rest/index.php
@@ -7,5 +7,16 @@ Flight::register('rmuService', 'RMUservice');
 
 require './routes/RMUroutes.php';
 
+Flight::route('GET /api/docs.json', function(){
+    $openapi = \OpenApi\scan('routes');
+    header('Content-Type: application/json');
+    echo $openapi->toJson();
+});
+
+Flight::map('header', function($name){
+    $headers = getallheaders();
+    return @$headers[$name];
+});
+
 Flight::start();
  ?>
---
+++ rest/routes/ApiSpec.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @OA\Info(title="RateMyUnivesrity", version="0.1", @OA\Contact(email="ahmedcosovic6@gmail.com", name="AhmedCosovic"))
+ * @OA\OpenApi(
+ *    @OA\Server(url="http://localhost/RateMyUniversity/rest", description="Development Environment" )
+ * ),
+ * @OA\SecurityScheme(securityScheme="ApiKeyAuth", type="apiKey", in="header", name="Authorization" )
+ */
+
+?>
