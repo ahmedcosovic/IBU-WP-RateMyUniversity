e32515ce1e5cc1f4519a6c9a46a3a5e31584d0c1
AhmedCosovic <ahmed.cosovic@stu.ibu.edu.ba>
2023-07-12 17:31:15 +0200

    production

--- rest/config.php
+++ rest/config.php
@@ -3,19 +3,19 @@
 class Config{
 
   public static function DB_HOST(){
-    return Config::get_env("DB_HOST", "localhost");
+    return Config::get_env("DB_HOST", "db-mysql-nyc1-71808-do-user-14370278-0.b.db.ondigitalocean.com");
   }
   public static function DB_USERNAME(){
-    return Config::get_env("DB_USERNAME", "root");
+    return Config::get_env("DB_USERNAME", "doadmin");
   }
   public static function DB_PASSWORD(){
-    return Config::get_env("DB_PASSWORD", "");
+    return Config::get_env("DB_PASSWORD", "AVNS_SieIIEElOG1zCHaqqfD");
   }
   public static function DB_SCHEMA(){
     return Config::get_env("DB_SCHEMA", "rmu");
   }
   public static function DB_PORT(){
-    return Config::get_env("DB_PORT", "3306");
+    return Config::get_env("DB_PORT", "25060");
   }
   public static function JWT_SECRET(){
     return "some_secret";
--- rest/routes/ApiSpec.php
+++ rest/routes/ApiSpec.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * @OA\Info(title="RateMyUniversity", version="0.1", @OA\Contact(email="ahmedcosovic6@gmail.com", name="AhmedCosovic"))
+ * @OA\Info(title="RateMyUniversity", version="0.1", @OA\Contact(email="ahmedcosovic6@gmail.com", name="development team"))
  * @OA\OpenApi(
- *    @OA\Server(url="http://localhost/RateMyUniversity/rest/api", description="Development Environment" )
+ *    @OA\Server(url="https://rate-my-university-8ef63f3b9c9d.herokuapp.com/rest/api", description="Production Environment" )
  * ),
  * @OA\SecurityScheme(securityScheme="ApiKeyAuth", type="apiKey", in="header", name="Authorization" )
  */
